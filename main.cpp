802d99ccf9708943f33e4af45b03052eba871d22
Jack Franco <jfranco@stevens.edu>
2023-04-18 23:23:26 -0400

    added test function for misc/cascade.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "CoolProp.h"
-#include "misc/test.h"
 #include "misc/cascade.cpp"
 
-int main(){
-    std::cout << "The NBP of water is: " << CoolProp::PropsSI("T","P",101325,"Q",0,"Water") << " K" << std::endl;
-    std::cout << "The NBP of water is: " << CoolProp::PropsSI("T","P",101325,"Q",0,"Water") << " K" << std::endl;
+using namespace std;
+using namespace CoolProp;
 
-    cout << CONST << endl;
+int main(){
+    cout << "Welcome to thermosim!" << endl;
+    
+    // Test function to run code in misc/cascade.cpp
+    displayCascade();
 
 }
